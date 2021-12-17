c132569270e296aeb99d042136df9865309dd11e
Mashanz <mangatkk@gmail.com>
2021-12-17 17:06:23 +0700

    [UPD] images

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -13,8 +13,8 @@ function Portfolio() {
         <section className="bg-gray-800">
             <h1 className="font-mono text-center py-4 text-yellow-500 font-bold text-2xl">./projects</h1>
             <div className="grid gap-2 grid-cols-1 md:grid-cols-2 md:max-w-2xl md:mx-auto mx-4">
-                <PortfolioItem title="Fastrack Academy" description="Learn to Become Full Stack Developer" image="./mashanz_logo.png" url="https://fastrack.mashanz.com" />
-                <PortfolioItem title="Expanse Tracker" description="Track Your Daily Spanding Easily" image="./mashanz_logo.png" url="https://fastrack.mashanz.com" />
+                <PortfolioItem title="Fastrack Academy" description="Learn to Become Full Stack Developer" image="https://fastrack.mashanz.com/static/course/meta.png" url="https://fastrack.mashanz.com" />
+                <PortfolioItem title="Expanse Tracker" description="Track Your Daily Spanding Easily" image="https://user-images.githubusercontent.com/9214059/138811707-abce8f65-4209-483a-a18a-e67ade70216a.png" url="https://fastrack.mashanz.com" />
                 <PortfolioItem title="On Demand Requests" description="Request Anything You Want" image="./mashanz_logo.png" url="https://fastrack.mashanz.com" />
             </div>
         </section>
