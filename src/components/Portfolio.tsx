d3870a5d8377d0e1ce24a8fd04fef9ac716a7c5e
Mashanz <mangatkk@gmail.com>
2022-01-01 15:29:59 +0700

    [UPD] link url

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,4 @@
-function PortfolioItem({ title='Title', description='description', image='/', url='#' }) {
+function PortfolioItem({ title = 'Title', description = 'description', image = '/', url = '#' }) {
     return (
         <a className="bg-gray-100 max-w-sm m-1 rounded-lg" href={url}>
             <img className="object-cover w-full h-32 rounded-t-lg" src={image} alt="" />
@@ -14,8 +14,8 @@ function Portfolio() {
             <h1 className="font-mono text-center py-4 text-yellow-500 font-bold text-2xl">./projects</h1>
             <div className="grid gap-2 grid-cols-1 md:grid-cols-2 md:max-w-2xl md:mx-auto mx-4">
                 <PortfolioItem title="Fastrack Academy" description="Learn to Become Full Stack Developer" image="https://fastrack.mashanz.com/static/course/meta.png" url="https://fastrack.mashanz.com" />
-                <PortfolioItem title="Expanse Tracker" description="Track Your Daily Spanding Easily" image="https://user-images.githubusercontent.com/9214059/138811707-abce8f65-4209-483a-a18a-e67ade70216a.png" url="https://fastrack.mashanz.com" />
-                <PortfolioItem title="On Demand Requests" description="Request Anything You Want" image="./mashanz_logo.png" url="https://fastrack.mashanz.com" />
+                <PortfolioItem title="Expanse Tracker" description="Track Your Daily Spanding Easily" image="https://user-images.githubusercontent.com/9214059/138811707-abce8f65-4209-483a-a18a-e67ade70216a.png" url="https://mashanz.com/logbook-django/" />
+                <PortfolioItem title="On Demand Requests" description="Request Anything You Want" image="./mashanz_logo.png" url="https://mashanz.com" />
             </div>
         </section>
     )
